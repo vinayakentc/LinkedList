a4fc81ee2406f4fef2ca954f7afe794bef663927
vinayakentc <vinayakchavanentc@gmail.com>
2021-02-28 01:26:25 +0530

    create linked list by add on top

--- src/com/linkedlist/LinkedListTest.java
+++ src/com/linkedlist/LinkedListTest.java
@@ -19,4 +19,19 @@ public class LinkedListTest {
 		boolean res = myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
 		Assert.assertTrue(res);
 	}
+
+	// UC2 TestCase
+	@Test
+	public void added_3NumbersAtTop() {
+		MyNode<Integer> myFirstNode = new MyNode<Integer>(70);
+		MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+		MyNode<Integer> myThirdNode = new MyNode<Integer>(56);
+		MyLinkedList lst = new MyLinkedList();
+		lst.add(myFirstNode);
+		lst.add(mySecondNode);
+		lst.add(myThirdNode);
+		boolean res = MyLinkedList.head.equals(myThirdNode) && MyLinkedList.head.getNext().equals(mySecondNode)
+				&& MyLinkedList.tail.equals(myFirstNode);
+		Assert.assertTrue(res);
+	}
 }
